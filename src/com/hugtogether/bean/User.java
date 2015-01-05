24eedf12ea3c37e55a78aa9f81ef802c58007409
luofuyun <luofuyun@luofuyun-OptiPlex-3020>
2015-01-05 17:41:38 +0800

    20150105

--- src/com/hugtogether/bean/User.java
+++ src/com/hugtogether/bean/User.java
@@ -12,8 +12,15 @@ import java.io.Serializable;
 */ 
 public class User implements Serializable {
 	private static final long serialVersionUID = -4517944185378137924L;
+	private Integer id;
 	private String userName;
 	private String password;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 	public String getUserName() {
 		return userName;
 	}
